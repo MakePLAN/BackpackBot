ce4352e8be0e42ebce690b5b9810e1414ecfb4f8
Sang Woon Lee <sangwoon@usc.edu>
2016-04-11 18:01:59 -0700

    Fixing skype

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const fs = require('fs');
 const restify = require('restify');
 const skype = require('skype-sdk');
 
+process.env.APP_ID = '2f803c4a-fb46-44ef-b974-742752bf9f3f';
+process.end.APP_SECRET = 'vVi5ZGMUOn6NvJAGXr1DT9s';
+
 const botService = new skype.BotService({
     messaging: {
         botId: '28:340042c3-9165-4b28-b4f1-2a051c7cccc6',
         serverUrl : "https://apis.skype.com ",
         requestTimeout : 15000,
-        appId: '2f803c4a-fb46-44ef-b974-742752bf9f3f',
-        appSecret: 'vVi5ZGMUOn6NvJAGXr1DT9s'
+        appId: process.env.APP_ID,
+        appSecret: process.env.APP_SECRET
     }
 });
 
