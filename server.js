f3d8e94e29f6f5641d7fc40871a2702842196ea3
Sang Woon Lee <sangwoon@usc.edu>
2016-04-12 00:40:49 -0700

    work

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const LineByLineReader = require('line-by-line');
 const restify = require('restify');
 const skype = require('skype-sdk');
 const HashMap = require('hashmap');
+const builder = require('botbuilder');
 
 
 var lr = new LineByLineReader('userlist.txt');
@@ -21,15 +22,6 @@ lr.on('line', function (line) {
 	map.set(arr[0], arr[1]);
 });
 
-/*
-fs.readFile('userlist.txt', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
-*/
-
 const botService = new skype.BotService({
     messaging: {
         botId: '28:340042c3-9165-4b28-b4f1-2a051c7cccc6',
@@ -40,7 +32,40 @@ const botService = new skype.BotService({
     }
 });
 
+// Create bot and add dialogs
+var bot = new builder.SkypeBot(botService);
+bot.add('/', function (session) {
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hi %s! What can I do for you?', session.userData.name);
+    }
+});
+bot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        //session.send('Hi %s! Nice to meet you. I am BackpackBot.', session.userData.name);
+        session.endDialog();
+    }
+]);
 //botService.
+/*
+botService.on('contactAdded', (bot, data) => {
+	//console.log(JSON.stringify(data) );
+	var str = data.from;
+	var arr = str.split(":");
+	var username = arr[1];
+	console.log(username + " added to the map!");
+
+	str = data.fromDisplayName;
+	arr = str.split(" ");
+	var name = arr[0];
+
+	//map.set(username, name);
+});
 
 botService.on('personalMessage', (bot, data) => {
 	var str = data.from;
@@ -50,24 +75,12 @@ botService.on('personalMessage', (bot, data) => {
 		bot.reply('Hi, ' + map.get(name) + '. What can I do for you?', true);
 	}
 	else{
-		bot.reply('Who are you? Fuck off', true);
+		//bot.reply('Who are you? Fuck off', true);
 	}
 
-	
-    
 });
-
-/*
-botService.add('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
-    }
-]);
 */
+
 
 const server = restify.createServer();
 server.post('/v1/chat', skype.messagingHandler(botService));
