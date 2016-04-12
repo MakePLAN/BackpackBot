961954f91fee3ce29c3f8bfb1e5679316c46064d
Sang Woon Lee <sangwoon@usc.edu>
2016-04-11 21:48:20 -0700

    test

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 
 const fs = require('fs');
+const LineByLineReader = require('line-by-line');
 const restify = require('restify');
 const skype = require('skype-sdk');
+const HashMap = require('hashmap');
+
+
+var lr = new LineByLineReader('userlist.txt');
+var map = new HashMap();
+
 
 process.env.APP_ID = '2f803c4a-fb46-44ef-b974-742752bf9f3f';
 process.env.APP_SECRET = 'vVi5ZGMUOn6NvJAGXr1DT9s';
+
+
+lr.on('line', function (line) {
+	// 'line' contains the current line without the trailing newline character.
+	console.log(line);
+	var arr = line.split(",");
+	map.set(arr[0], arr[1]);
+});
+
+/*
+fs.readFile('userlist.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
+*/
 
 const botService = new skype.BotService({
     messaging: {
@@ -16,13 +40,34 @@ const botService = new skype.BotService({
     }
 });
 
-botService.on('contactAdded', (bot, data) => {
-    bot.reply(`Hello ${data.fromDisplayName}!`, true);
-});
+//botService.
 
 botService.on('personalMessage', (bot, data) => {
-    bot.reply(`Hey ${data.from}. Thank you for your message: "${data.content}".`, true);
+	var str = data.from;
+	var arr = str.split(":");
+	var name = arr[1];
+	if (map.has(name) ){
+		bot.reply('Hi, ' + map.get(name) + '. What can I do for you?', true);
+	}
+	else{
+		bot.reply('Who are you? Fuck off', true);
+	}
+
+	
+    
 });
+
+/*
+botService.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+*/
 
 const server = restify.createServer();
 server.post('/v1/chat', skype.messagingHandler(botService));
